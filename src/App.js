bd33f6325a6f99b3664ac8ef2cba2942f970c552
Tobi Olasimbo <tobiolasimbo84@gmail.com>
2023-03-15 10:18:46 +0100

    home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react"
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div className="flex h-[100vh] justify-center items-center bg-[#000000]">
+     <ToastContainer
+        position="top-center"
+        className="w-full"
+        style={{ width: "600px" }}
+      /> 
+      
+      <HomePage/>
+      <Router>
+        <Routes>
+
+        </Routes>
+      </Router>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
+    screens:{
+      "xsm":"412px",
+      "sm":"540px",
+      "md":"768px",
+      "lg":"1024px",
+      "xl": "1280px",
+      "2xl": "1536px",
+    },
     extend: {},
   },
-  plugins: [require('@tailwindcss/forms'),require('tailwind-scrollbar-hide')],
+  plugins: [require('@tailwindcss/forms')],
 }
